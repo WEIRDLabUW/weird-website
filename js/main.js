652036ff4e7df8ef5d3a6bdb5ab9bccf574714d7
Arhan Jain <arhan.j04@gmail.com>
2025-12-24 01:48:39 -0800

    website vibe determined

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Mobile menu
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('nav');
+
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => nav.classList.toggle('open'));
+  }
+});
